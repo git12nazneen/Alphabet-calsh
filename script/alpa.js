28f6f4f678e5d745d4d64c94f296d7186e4f8279
git12nazneen <lipinazneen65@gmail.com>
2024-02-16 15:04:12 -0600

    calculate bonus

--- script/alpa.js
+++ script/alpa.js
@@ -4,17 +4,64 @@
 //     homeSection.classList.add('hidden');
 
 
-//     // show the playground  
-//     const playgroundSection = document.getAnimations('plays-ground');
-//     playgroundSection.classList.remove('hidden')
-// }
+
+function handleKeyboardkeyUpEvent(event){
+  const playerPressed = event.key;
+  // console.log('plauy',playerPressed)
+
+  // get the expected to press
+const currentAlphabetElement = document.getElementById('current-palace')
+const currentAlphabet = currentAlphabetElement.innerText;
+const expectedAlphabet = currentAlphabet.toLowerCase();
+// console.log(playerPressed, expectedAlphabet)
+
+// check match or not
+if(playerPressed === expectedAlphabet){
+  console.log('bonus');
+//  update score
+// 1 get current score  
+const currentSccoreElement = document.getElementById('current-score');
+const currentScoreText = currentSccoreElement.innerText;
+const currentScore = parseInt(currentScoreText)
+console.log(currentScore)
+// 2 increase the score 1
+const newScore = currentScore + 1;
+// 3 show the update score
+currentSccoreElement.innerText = newScore;
+  // start a new round
+  removeBackgroundColourById(expectedAlphabet)
+  continueGame()
+}
+else{
+  console.log('not bonus');
+  // strp 1: get the current life
+  const currentLifeElement = document.getElementById('current-life')
+  const currentLIfeText = currentLifeElement.innerText;
+  const currentLife = parseInt(currentLIfeText);
+  console.log(currentLife)
+  // 2:reduce the life
+  const newCurrentScore = currentLife - 1;
+  // 3: display update
+  currentLifeElement.innerText = newCurrentScore;
+
+  if(newCurrentScore === 0){
+    console.log('game over')
+  }
+
+}
+}
+
+
+// capture keyboard key press
+document.addEventListener('keyup', handleKeyboardkeyUpEvent);
+
 function continueGame(){
     //   step1: generate a random alphabet
       const alphabet = getaRandomAlphabet();
-      console.log('your random ', alphabet);
+      // console.log('your random ', alphabet);
 
       // set alphabet
-      const currentAlphabetElement = document.getElementById('current-alphabet');
+      const currentAlphabetElement = document.getElementById('current-palace');
       currentAlphabetElement.innerText = alphabet;
 
       // set background color
--- script/utility.js
+++ script/utility.js
@@ -12,12 +12,15 @@ function setBackgroundColorById(elementId){
   const element = document.getElementById(elementId);
   element.classList.add('bg-orange-400');
 }
-
+function removeBackgroundColourById(elementId){
+  const element = document.getElementById(elementId)
+  element.classList.remove('bg-orange-400')
+}
 function getaRandomAlphabet(){
     // get or ceeate a alphabet
   const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
   const alphabets = alphabetString.split('');
-  console.log(alphabets);
+  // console.log(alphabets);
     // random index between 0 - 25
     const randomNumber = Math.random() * 25;
     const index = Math.round(randomNumber);
